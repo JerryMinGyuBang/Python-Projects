710f08b52aed0e36b476d1889b59555e93c6bb85
Min Gyu Bang <mbang4@myseneca.ca>
2024-02-07 00:58:33 -0500

    exception handling functionality added

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,11 +1,20 @@
 import pandas
 
-
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(nato_dict)
 
 
-word = input("Enter a word: ").upper()
-word_list = [nato_dict[letter] for letter in word]
-print(word_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        word_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letter in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(word_list)
+
+
+generate_phonetic()
